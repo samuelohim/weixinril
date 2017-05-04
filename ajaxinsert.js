f59e864406c1cf2032bd84f326c90a0fc75fe850
Samuel Y <samuel.y@weimeit.com>
2017-05-04 10:18:50 +0800

    ajaxinsert.js renderWebViewer modulized & email and phone number insertable to DB

--- ajaxinsert.js
+++ ajaxinsert.js
@@ -1,40 +1,21 @@
 $(document).ready(function(){
 	$('#sub').click(function(){
-		alert('ln 3');
+		$.post($('#myf').attr('action'),
 
-		$.post($('#myf').attr('action'), 
-			$('#myf :input').serializeArray(), 
-			function(allEchos){
-				if (allEchos)
-				{
-					alert('ln 10 = '+allEchos);
-					$('#myf :input').val('');
-					var shchu = $.parseJSON(allEchos);
-					console.log('ln 13 = '+shchu);
-						$('table.listing').append('<tr id = \''+shchu['id']+'\'>\
-												<td class = \'wid\'>'+shchu['wid']+'</td>\
-												<td class = \'riq\'><span id = \'ri\'>'+shchu['riq']+'-</span>\
-																	<span id = \'yue\'>'+shchu['yue']+'-</span>\
-																	<span id = \'nin\'>'+shchu['nin']+'</span></td>\
-												<td class = \'shijian\'><span id = \'zht\'>'+shchu['xsh']+':</span>\
-																	<span id = \'fzh\'>'+shchu['fzh']+'</span>\
-																	<span id = \'zou\'>'+shchu['zou']+'</span></td>\
-												<td class = \'shijian\'><span id = \'leixing\'>'+shchu['lx']+'</span></td>\
-												<td class = \'settings\'><button class=\"btn btn-danger\">\
-																			<span class = \'glyphicon glyphicon-trash\'></span>\
-																		</button>\
-																		\
-																		<button class=\"btn btn-success\">\
-																			<span class = \'glyphicon glyphicon-zoom-in\'></span>\
-																		</button>\
-																		\
-																		<button class=\"btn btn-warning\">\
-																			<span class = \'glyphicon glyphicon-wrench\'></span>\
-																		</button></td>\
-												</tr>');
+			$('#myf :input').serializeArray(),
+
+					function(allEchos){
+						if (allEchos)
+						{
+							$('#myf :input').val('');
+							var shchu = $.parseJSON(allEchos);
+								console.log('ln 13 = '+shchu);
+							renderWebView(shchu);
+						}
+						del();
 				}
-				del();
-		});
+
+		);
 	
 			
 
@@ -45,5 +26,29 @@ $(document).ready(function(){
 
 	});
 
-
 });
+//------------------------------------- Functions ----------------------------------------------------------------------
+function renderWebView(shchu){
+	$('table.listing').append('<tr id = \''+shchu['id']+'\'>\
+		<td class = \'wid\'>'+shchu['wid']+'</td>\
+		<td class = \'riq\'><span id = \'ri\'>'+shchu['riq']+'-</span>\
+							<span id = \'yue\'>'+shchu['yue']+'-</span>\
+							<span id = \'nin\'>'+shchu['nin']+'</span></td>\
+		<td class = \'shijian\'><span id = \'zht\'>'+shchu['xsh']+':</span>\
+							<span id = \'fzh\'>'+shchu['fzh']+'</span>\
+							<span id = \'zou\'>'+shchu['zou']+'</span></td>\
+		<td class = \'shijian\'><span id = \'leixing\'>'+shchu['lx']+'</span></td>\
+		<td class = \'settings\'>\
+				<button class=\"btn btn-danger\">\
+					<span class = \'glyphicon glyphicon-trash\'></span>\
+				</button>\
+				\
+				<button class=\"btn btn-success\">\
+					<span class = \'glyphicon glyphicon-zoom-in\'></span>\
+				</button>\
+				\
+				<button class=\"btn btn-warning\">\
+					<span class = \'glyphicon glyphicon-wrench\'></span>\
+				</button></td>\
+		</tr>');
+}
--- insertdb.php
+++ insertdb.php
@@ -7,15 +7,17 @@
 		$riq = $_POST['riq'];
 		$yue = $_POST['yue'];
 		$nin = $_POST['nin'];
+		$dianyou = $_POST['dianyou'];
+		$dianhua = $_POST['dianhua'];
 		$zht = $_POST['xsh'];
 		$fzh = $_POST['fzh'];
 		$lexing = $_POST['lx'];
 		$zou = $_POST['zou'];
 //  
 
-	if (mysqli_query($con, "INSERT INTO yuyue (wid, riq, yue, nin, zht, 
+	if (mysqli_query($con, "INSERT INTO yuyue (wid, riq, yue, nin, zht, dianyou, dianhua, 
 												fzh, zou, leixing) 
-							VALUES ('$wid', '$riq', '$yue', '$nin', '$zht',
+							VALUES ('$wid', '$riq', '$yue', '$nin', '$zht', '$dianyou', '$dianhua',
 									 '$fzh', '$zou', '$lexing')"))
 	{
 		 	echo json_encode($_POST);	
